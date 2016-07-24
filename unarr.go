c384ffd50c6e9f100ff5c45086db74f9e9c42230
Matthias Brooks <matthias.brooks@live.com>
2016-07-24 17:26:52 -0400

    Update unarr.go

--- unarr.go
+++ unarr.go
@@ -10,6 +10,9 @@ import (
 	"strings"
 	"time"
 	"unsafe"
+	"io/ioutil"
+	"os"
+	"fmt"
 )
 
 // Archive represents unarr archive
@@ -126,4 +129,54 @@ func (a *Archive) ModTime() time.Time {
 func (a *Archive) Close() {
 	C.ar_close_archive(a.archive)
 	C.ar_close(a.stream)
+}
+
+func ExtractArchive(filename string, dest string) {
+	archive, err := NewArchive(filename)
+	if err != nil {
+		fmt.Printf("Error NewArchive: %s\n", err)
+		return
+	}
+	defer archive.Close()
+
+	for {
+		err := archive.Entry()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Printf("Error Entry: %s\n", err)
+				break
+			}
+		}
+
+		name := archive.Name()
+		size := archive.Size()
+		mtime := archive.ModTime()
+
+		fmt.Printf("%d\t%s %s\t%s\n", size, mtime.Format("02-01-06"), mtime.Format("15:04"), name)
+
+		buf := make([]byte, size)
+		for size > 0 {
+			n, err := archive.Read(buf)
+			if err != nil && err != io.EOF {
+				break
+			}
+			size -= n
+		}
+
+		if size > 0 {
+			fmt.Printf("Error Read\n")
+			continue
+		}
+
+		dirname := filepath.Join(dest, filepath.Dir(name))
+		os.MkdirAll(dirname, 0755)
+
+		err = ioutil.WriteFile(filepath.Join(dirname, filepath.Base(name)), buf, 0644)
+		if err != nil {
+			fmt.Printf("Error WriteFile: %s\n", err)
+			continue
+		}
+	}
 }
